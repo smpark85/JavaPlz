a1185c81c10e8c6ca644b5ec4e0c16e6afd037df
smpark85 <80630779+smpark85@users.noreply.github.com>
2023-03-23 14:06:46 +0900

    Update HelloWorld.java

--- src/part1/ch02/ex2_5/HelloWorld.java
+++ src/part1/ch02/ex2_5/HelloWorld.java
@@ -2,10 +2,10 @@ package part1.ch02.ex2_5;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String message = "헬로 월드!";
-        System.out.println(message);
+        String message = "헬로 월드!"; //변수 생성 및 초기화
+        System.out.println(message); // 출력할 변수명
 
-        message = "웰컴 투 헬!";
-        System.out.println(message);
+        message = "웰컴 투 헬!"; //새로 대입할 값
+        System.out.println(message); //출력할 변수명 (바뀐값이 출력됨)
     }
 }
