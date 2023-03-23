6d4c418208bbc79f6f426b0d404cc5fd35956817
smpark85 <80630779+smpark85@users.noreply.github.com>
2023-03-23 14:04:14 +0900

    Update TangSoo6.java

--- src/part1/ch02/ex2_4/TangSoo6.java
+++ src/part1/ch02/ex2_4/TangSoo6.java
@@ -2,10 +2,10 @@ package part1.ch02.ex2_4;
 
 public class TangSoo6 {
     public static void main(String[] args) {
-        boolean isBoomuk;
-        isBoomuk = true;
+        boolean isBoomuk;  //논리형 변수 생성
+        isBoomuk = true; // 대입할 논릿값
 
         System.out.println("당신은 부먹파입니까?");
-        System.out.println(isBoomuk);
+        System.out.println(isBoomuk);  //출력할 변수명
     }
 }
