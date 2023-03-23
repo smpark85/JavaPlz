1069a30828a0d3a1071cce9f21ec6c3536f8b56b
smpark85 <80630779+smpark85@users.noreply.github.com>
2023-03-23 14:36:07 +0900

    Update TypeMismatch.java

--- src/part1/ch02/ex2_10/TypeMismatch.java
+++ src/part1/ch02/ex2_10/TypeMismatch.java
@@ -4,8 +4,8 @@ public class TypeMismatch {
     public static void main(String[] args) {
         // 변수 생성
         String title = "로미오와 줄리엣";
-//        int author = "윌리엄 셰익스피어"; //타입 불일치 코드
-//        int price = 5.94; //타입 불일치 코드
+//        int author = "윌리엄 셰익스피어"; //타입 불일치 코드 int -> String
+//        int price = 5.94; //타입 불일치 코드 int -> double
 
         // 결과 출력
         System.out.printf("제목: %s\n ", title);  //\n 줄 개행을 의미 (enter키 효과)
