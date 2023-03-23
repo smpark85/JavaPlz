8bc4a60f0fc37904b004eb85706c5d1caf2c379c
smpark85 <80630779+smpark85@users.noreply.github.com>
2023-03-23 14:01:19 +0900

    Update Exam.java

--- src/part1/ch02/ex2_2/Exam.java
+++ src/part1/ch02/ex2_2/Exam.java
@@ -2,14 +2,14 @@ package part1.ch02.ex2_2;
 
 public class Exam {
     public static void main(String[] args) {
-        int midScore;    // 중간 점수
+        int midScore;    // 중간 점수, 정수형 변수 생성
         int finalScore;  // 기말 점수
 
-        midScore = 68;
+        midScore = 68; // 변수에 대입할 값
         finalScore = 88;
 
         System.out.println("학기 전 홍팍이의 중간 점수:");
-        System.out.println(midScore);
+        System.out.println(midScore); //출력할 변수명
         System.out.println("학기 전 홍팍이의 기말 점수:");
         System.out.println(finalScore);
     }
