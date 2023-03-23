b27064a94dd6e402b8306a523955ef7f2489ea04
smpark85 <80630779+smpark85@users.noreply.github.com>
2023-03-23 13:58:52 +0900

    Update Greetings2.java

--- src/part1/ch02/ex2_1/Greetings2.java
+++ src/part1/ch02/ex2_1/Greetings2.java
@@ -2,11 +2,11 @@ package part1.ch02.ex2_1;
 
 public class Greetings2 {
     public static void main(String[] args) {
-        String name;
-        name = "김한빛";
+        String name;    //문자열 변수 name 생성
+        name = "김한빛"; //문자열 값 저장 (대입)
 
         System.out.println("안녕하세요~ 반갑습니다! 제 이름은,");
-        System.out.println(name);
+        System.out.println(name);  //출력할 변수명
         System.out.println("입니다. 잘 부탁드립니다~");
     }
 }
