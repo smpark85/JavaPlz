99725daaf722bec1712546174a093bb834ac1609
smpark85 <80630779+smpark85@users.noreply.github.com>
2023-03-23 14:29:34 +0900

    Update Arithmetic.java

--- src/part1/ch02/ex2_9/Arithmetic.java
+++ src/part1/ch02/ex2_9/Arithmetic.java
@@ -2,8 +2,8 @@ package part1.ch02.ex2_9;
 
 public class Arithmetic {
     public static void main(String[] args) {
-        int x = 7 / 2;
-        double y = 7 * 2.0;
-        System.out.printf("x = %d, y = %f", x, y);
+        int x = 7 / 2; //정수와 정수의 연산
+        double y = 7 * 2.0; //정수와 실수의 연산
+        System.out.printf("x = %d, y = %f", x, y); //출력될 형식 문자열, 형식 문자열에 삽입될 변수값
     }
 }
