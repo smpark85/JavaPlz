5891dc08a00a1415c4b2e4d58d5cead612a5bdca
smpark85 <80630779+smpark85@users.noreply.github.com>
2023-03-23 14:37:20 +0900

    Update DownCasting.java

--- src/part1/ch02/ex2_11/DownCasting.java
+++ src/part1/ch02/ex2_11/DownCasting.java
@@ -4,7 +4,7 @@ public class DownCasting {
     public static void main(String[] args) {
         double tall = 176.6;
         double weight = 74.34;
-        System.out.printf("신장: %dcm\n", tall);
+        System.out.printf("신장: %dcm\n", tall); //정수값이 삽입될 곳, 실수값을 입력함
         System.out.printf("체중: %dkg\n", weight);
     }
 }
