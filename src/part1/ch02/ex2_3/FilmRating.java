ba00519811ac209e94030b3f5f95057d00cb53ee
smpark85 <80630779+smpark85@users.noreply.github.com>
2023-03-23 14:03:02 +0900

    Update FilmRating.java

--- src/part1/ch02/ex2_3/FilmRating.java
+++ src/part1/ch02/ex2_3/FilmRating.java
@@ -2,10 +2,10 @@ package part1.ch02.ex2_3;
 
 public class FilmRating {
     public static void main(String[] args) {
-        double rating;
-        rating = 9.82;
+        double rating;  //실수형 변수 생성
+        rating = 9.82; // 변수에 대입할 값
 
         System.out.println("바람과 함께 사라지다 평점:");
-        System.out.println(rating);
+        System.out.println(rating);  //출력할 변수명
     }
 }
